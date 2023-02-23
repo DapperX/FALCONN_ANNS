c9d27f05fdc64bbaf68349ce0996988250bf99d7
Zheqi Shen <zheqi.shen@email.ucr.edu>
2023-02-23 03:47:55 -0800

    Support parallel query and auto tune num_probes

--- src/examples/ANNtest/calc_recall.cpp
+++ src/examples/ANNtest/calc_recall.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 #include <cstdint>
 #include <algorithm>
@@ -61,67 +62,46 @@ public:
 	}
 };
 
-template<class U, class V>
-void output_recall(LSHNearestNeighborTable<U,uint32_t> &tbl, parlay::internal::timer &t, uint32_t num_probe, uint32_t recall, 
+// Visit all the vectors in the given 2D array of points
+// This triggers the page fetching if the vectors are mmap-ed
+template<class T>
+void visit_point(const T &array, size_t dim0, size_t dim1)
+{
+	parlay::parallel_for(0, dim0, [&](size_t i){
+		const auto &a = array[i];
+		[[maybe_unused]] volatile auto elem = a[0];
+		for(size_t j=1; j<dim1; ++j)
+			elem = a[j];
+	});
+}
+
+template<typename T, class U, class V>
+size_t output_recall(LSHNearestNeighborTable<U,uint32_t> &tbl, parlay::internal::timer &t, uint32_t num_probes, uint32_t recall, 
 	uint32_t cnt_query, std::vector<V> &q, std::vector<uint32_t*> &gt, uint32_t rank_max)
 {
-	//std::vector<std::vector<std::pair<uint32_t,float>>> res(cnt_query);
+	std::unique_ptr<falconn::LSHNearestNeighborQueryPool<U,uint32_t>> query_pool(tbl.construct_query_pool(num_probes));
+
 	parlay::sequence<std::vector<uint32_t>> res(cnt_query);
-	/* // TODO
 	parlay::parallel_for(0, cnt_query, [&](size_t i){
-		res[i] = g.search(q[i], recall, num_probe);
+		// query_pool->find_k_nearest_neighbors(q[i], recall, &res[i]);
+		query_pool->get_candidates_with_duplicates(q[i], &res[i]);
 	});
-	*/
 	t.next("Warm-up search");
-	 // TODO
-	auto query_object = tbl.construct_query_object(num_probe);
-
-	// TODO
-	/*
-	parlay::parallel_for(0, cnt_query, [&](size_t i){
-		// flag_query
-		// search_control ctrl{};
-		using PointType = typename to_densevec<float>::type;
-		auto qobj = *dynamic_cast<
-
-			falconn::wrapper::LSHNNQueryWrapper<
-				PointType,
-				uint32_t,
-				float,
-				falconn::core::StaticLSHTable<PointType, KeyType, LSHType, HashType,CompositeHashTableType, DataStorageType>,
-				float,
-				falconn::wrapper::PointTypeTraitsInternal<PointType>::CosineDistance,
-				DataStorage
-			>
-
-			falconn::wrapper::LSHNNQueryWrapper<Eigen::Matrix<float, -1, 1>, int, float, falconn::core::StaticLSHTable<Eigen::Matrix<float, -1, 1>, unsigned int, falconn::core::CrossPolytopeHashDense<float, long unsigned int>, long unsigned int, falconn::core::StaticCompositeHashTable<long unsigned int, unsigned int, falconn::core::StaticLinearProbingHashTable<long unsigned int, unsigned int> >, falconn::core::ArrayDataStorage<Eigen::Matrix<float, -1, 1>, unsigned int> >, float, falconn::core::EuclideanDistanceDense<float>, falconn::core::ArrayDataStorage<Eigen::Matrix<float, -1, 1>, unsigned int> >*
-		>(query_object.get());
-		qobj.find_k_nearest_neighbors(q[i], recall, &res[i]);
-	});
-	*/
 
 	for(size_t i=0; i<cnt_query; ++i)
 	{
-		// flag_query
-		// search_control ctrl{};
-		// query_object->find_k_nearest_neighbors(q[i], recall, &res[i]);
-		query_object->get_candidates_with_duplicates(q[i], &res[i]);
+		// query_pool->find_k_nearest_neighbors(q[i], recall, &res[i]);
+		query_pool->get_candidates_with_duplicates(q[i], &res[i]);
 	};
 	
-	//auto t2 = std::chrono::high_resolution_clock::now();
 	double time_query = t.next_time();
-	//printf("time diff: %.8f\n", time_query);
-	// auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1);
-	//std::chrono::duration<double, std::milli> diff = t2-t1;
-	//printf("time diff (hi): %.4f\n", diff.count());
-	// t.report(time_query, "Find neighbors");
 	printf("FALCONN: Find neighbors: %.4f\n", time_query);
 
 	if(rank_max<recall)
 		recall = rank_max;
 //	uint32_t cnt_all_shot = 0;
 	std::vector<uint32_t> result(recall+1);
-	printf("measure recall@%u with num_probe=%u on %u queries\n", recall, num_probe, cnt_query);
+	printf("measure recall@%u with num_probes=%u on %u queries\n", recall, num_probes, cnt_query);
 	for(uint32_t i=0; i<cnt_query; ++i)
 	{
 		uint32_t cnt_shot = 0;
@@ -134,83 +114,123 @@ void output_recall(LSHNearestNeighborTable<U,uint32_t> &tbl, parlay::internal::t
 		result[cnt_shot]++;
 	}
 	// printf("#all shot: %u (%.2f)\n", cnt_all_shot, float(cnt_all_shot)/cnt_query);
-	uint32_t cnt_shot = 0;
+	uint32_t total_shot = 0;
 	for(uint32_t i=0; i<=recall; ++i)
 	{
 		printf("%u ", result[i]);
-		cnt_shot += result[i]*i;
+		total_shot += result[i]*i;
 	}
 	putchar('\n');
-	printf("%.6f at %ekqps\n", float(cnt_shot)/cnt_query/recall, cnt_query/time_query/1000);
+	printf("%.6f at %ekqps\n", float(total_shot)/cnt_query/recall, cnt_query/time_query/1000);
 	/* // TODO
 	printf("# visited: %lu\n", g.total_visited.load());
 	printf("# eval: %lu\n", g.total_eval.load());
 	printf("size of C: %lu\n", g.total_size_C.load());
 	*/
-	auto statistics = query_object->get_query_statistics();
-	cout << "average total query time: " << statistics.average_total_query_time << endl;
-	cout << "average lsh time: " << statistics.average_lsh_time << endl;
-	cout << "average hash table time: " << statistics.average_hash_table_time << endl;
-	cout << "average distance time: " << statistics.average_distance_time << endl;
-	cout << "average number of candidates: " << statistics.average_num_candidates << endl;
-	cout << "average number of unique candidates: " << statistics.average_num_unique_candidates << endl;
+	falconn::QueryStatistics stats = query_pool->get_query_statistics();
+	cout << "average total query time: " << stats.average_total_query_time << endl;
+	cout << "average lsh time: " << stats.average_lsh_time << endl;
+	cout << "average hash table time: " << stats.average_hash_table_time << endl;
+	cout << "average distance time: " << stats.average_distance_time << endl;
+	cout << "average number of candidates: " << stats.average_num_candidates << endl;
+	cout << "average number of unique candidates: " << stats.average_num_unique_candidates << endl;
 	puts("---");
+	return total_shot;
 }
 
-template<class U>
-void output_recall(LSHNearestNeighborTable<U,uint32_t> &tbl, commandLine param, parlay::internal::timer &t)
+template<typename T, class U>
+void output_recall(LSHNearestNeighborTable<U,uint32_t> &tbl, commandLine param, parlay::internal::timer &t, uint32_t dim, uint32_t L)
 {
-	if(param.getOption("-?"))
-	{
-		printf(__func__);
-		puts(
-			"[-q <queryFile>] [-g <groundtruthFile>]"
-			"-ef <ef_query> [-r <recall@R>=1] [-k <numQuery>=all]"
-		);
-		return;
-	};
-	char* file_query = param.getOptionValue("-q");
-	char* file_groundtruth = param.getOptionValue("-g");
-	auto [q,_] = load_point(file_query, to_point<float>); // TODO
+	const char* file_query = param.getOptionValue("-q");
+	const char* file_groundtruth = param.getOptionValue("-g");
+	auto [q,_] = load_point(file_query, to_point<T>);
 	t.next("Read queryFile");
+	printf("%s: [%lu,%u]\n", file_query, q.size(), _);
 
-	uint32_t cnt_rank_cmp = param.getOptionIntValue("-r", 1);
-//	const uint32_t ef = param.getOptionIntValue("-ef", cnt_rank_cmp*50);
-	const uint32_t cnt_pts_query = param.getOptionIntValue("-k", q.size());
+	visit_point(q, q.size(), dim); // TODO: eliminate the passed dim
+	t.next("Fetch query vectors");
 
 	auto [gt,rank_max] = load_point(file_groundtruth, gt_converter<uint32_t>{});
-	for(uint32_t i=100; i<6000; i+=500)
-		output_recall(tbl, t, i, cnt_rank_cmp, cnt_pts_query, q, gt, rank_max);
+	t.next("Read groundTruthFile");
+	printf("%s: [%lu,%u]\n", file_groundtruth, gt.size(), rank_max);
+
+	auto parse_array = [](const std::string &s, auto f){
+		std::stringstream ss;
+		ss << s;
+		std::string current;
+		std::vector<decltype(f((char*)NULL))> res;
+		while(std::getline(ss, current, ','))
+			res.push_back(f(current.c_str()));
+		std::sort(res.begin(), res.end());
+		return res;
+	};
+	const uint32_t cnt_query = param.getOptionIntValue("-k", q.size());
+	auto cnt_rank_cmp = parse_array(param.getOptionValue("-r"), atoi);
+	auto threshold = parse_array(param.getOptionValue("-th"), atof);
+
+	auto get_best = [&](uint32_t k, uint32_t num_probes){
+		return output_recall<T>(tbl, t, num_probes, k, cnt_query, q, gt, rank_max);
+	};
+
+	for(auto k : cnt_rank_cmp)
+	{
+		uint32_t l_last = L;
+		for(auto t : threshold)
+		{
+			printf("searching for k=%u, th=%f\n", k, t);
+			const size_t target = t*cnt_query*k;
+			uint32_t l=l_last, r_limit=k*150;
+			uint32_t r = l;
+			bool found = false;
+			while(true)
+			{
+				// auto [best_shot, best_beta] = get_best(k, r);
+				if(get_best(k,r)>=target)
+				{
+					found = true;
+					break;
+				}
+				if(r==r_limit) break;
+				r = std::min(r*2, r_limit);
+			}
+			if(!found) break;
+			while(r-l>5)
+			{
+				const auto mid = (l+r)/2;
+				const auto best_shot = get_best(k,mid);
+				if(best_shot>=target)
+					r = mid;
+				else
+					l = mid;
+			}
+			l_last = l;
+		}
+	}
 }
 
-template<typename U>
+template<typename T>
 void run_test(commandLine args) // intend to pass by value
 {
-	using type_point = typename to_densevec<U>::type;
+	using type_point = typename to_densevec<T>::type;
 
 	const char* file_in = args.getOptionValue("-in");
 	const uint32_t cnt_points = args.getOptionLongValue("-n", 0);
-	// const float m_l = args.getOptionDoubleValue("-ml", 0.36);
-	const uint32_t num_hashtbl = args.getOptionIntValue("-l", 40);
-	const uint32_t num_hashbit = args.getOptionIntValue("-b", 18);
-	const uint32_t num_rotations = args.getOptionIntValue("-rot", 1);
-	const auto lsh_family = args.getOptionValue("-lsh", "cp");
-	const auto dist_func = args.getOptionValue("-dist", "L2");
-	// const bool do_fixing = !!args.getOptionIntValue("-f", 0);
-	// flag_query = args.getOptionIntValue("-flag", 0);
 
 	parlay::internal::timer t("FALCONN", true);
-
-	// using T = typename U::type_elem; // TODO
-	auto [ps,dim] = load_point(file_in, to_point<U/*// TODO*/>, cnt_points);
+	auto [ps,dim] = load_point(file_in, to_point<T>, cnt_points);
 	t.next("Read inFile");
 	printf("col: %lu\n", ps[0].cols());
 	printf("row: %lu\n", ps[0].rows());
 	printf("size: %lu\n", ps[0].size());
 
-	 // TODO
+	const uint32_t num_hashtbl = args.getOptionIntValue("-l", 50); // 40~80
+	const uint32_t num_rotations = args.getOptionIntValue("-rot", 1);
+	const auto lsh_family = args.getOptionValue("-lsh", "cp");
+	const auto dist_func = args.getOptionValue("-dist", "L2");
+	const uint32_t num_hashbit = args.getOptionIntValue("-b", ceil(log2(ps.size()))-2);
+	printf("num_hashbit: %u\n", num_hashtbl);
+
 	falconn::LSHConstructionParameters params;
-	// TODO: get default parameter
 	if(lsh_family=="cp")
 		params.lsh_family = falconn::LSHFamily::CrossPolytope;
 	else if(lsh_family=="hp")
@@ -233,13 +253,7 @@ void run_test(commandLine args) // intend to pass by value
 	auto ptbl = falconn::construct_table<type_point,uint32_t>(ps, params); // TODO
 	t.next("Build index");
 
-	/* // TODO
-	size_t cnt_degree = g.cnt_degree();
-	printf("total degree: %lu\n", cnt_degree);
-	t.next("Count degrees");
-	*/
-
-	output_recall(*ptbl, args, t);
+	output_recall<T>(*ptbl, args, t, dim, num_hashtbl);
 }
 
 int main(int argc, char **argv)
@@ -249,15 +263,14 @@ int main(int argc, char **argv)
 	putchar('\n');
 
 	commandLine parameter(argc, argv, 
-		"-type <elemType> -dist <distance> -n <numInput> -ml <m_l> -m <m> "
-		"-efc <ef_construction> -alpha <alpha> -r <recall@R> [-b <batchBase>]"
-		"-in <inFile> ..."
+		"-type <elemType> -n <numInput> -r <recall@R>,... -th <threshold>,..."
+		"-in <inFile> -q <queryFile> -g <groundtruthFile> [-k <numQuery>=all] "
+		"-dist (L2|ndot) -lsh (cp|hp) -l <numHashTable> [-b <numHashBit>] [-rot <numRotation>]"
 	);
 
-	/* // TODO
-	const char* dist = parameter.getOptionValue("-dist");
 	const char* type = parameter.getOptionValue("-type");
-	*/
-	run_test<float>(parameter);
+	if(!strcmp(type,"float"))
+		run_test<float>(parameter);
+	else throw std::invalid_argument("Unsupported element type");	
 	return 0;
 }
